62f126786282b440528d795e078e82dcaa51e4c3
Mallikarjun S <mallusalimath203@gmail.com>
2019-10-24 16:20:33 +0530

    updated project structuer 24/10/2019

--- src/main/java/resources/PieChart_AWT.java
+++ src/main/java/resources/PieChart_AWT.java
@@ -1,15 +1,14 @@
 package resources;
 
-import javax.swing.JPanel;
-
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 import org.jfree.data.general.DefaultPieDataset;
 import org.jfree.data.general.PieDataset;
 import org.jfree.ui.ApplicationFrame;
-import org.jfree.ui.RefineryUtilities;
- 
+
+import javax.swing.*;
+
 public class PieChart_AWT extends ApplicationFrame {
 	
 	static double p;
